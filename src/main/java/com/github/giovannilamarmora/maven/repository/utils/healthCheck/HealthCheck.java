719dafef2ce290624fe316b1f1050a0593aa4b6b
Giovanni Lamarmora <giovannilamarmora.working@gmail.com>
2022-07-27 15:17:33 +0200

    added HealthCheck

---
+++ src/main/java/com/github/giovannilamarmora/maven/repository/utils/healthCheck/HealthCheck.java
@@ -0,0 +1,8 @@
+package com.github.giovannilamarmora.maven.repository.utils.healthCheck;
+
+public class HealthCheck {
+
+    public static String startHealthCheck(){
+        return "STATUS_OK";
+    }
+}
