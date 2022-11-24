b845c64800f20070bf399c70036cafbe560be205
Giovanni Lamarmora <giovannilamarmora.working@gmail.com>
2022-11-24 16:58:21 +0100

    feat(): Added ExceptionMessage

--- src/main/java/io/github/giovannilamarmora/utils/exception/UtilsException.java
+++ src/main/java/io/github/giovannilamarmora/utils/exception/UtilsException.java
@@ -3,6 +3,7 @@ package io.github.giovannilamarmora.utils.exception;
 import io.github.giovannilamarmora.utils.exception.dto.ErrorInfo;
 import io.github.giovannilamarmora.utils.exception.dto.ExceptionResponse;
 import io.github.giovannilamarmora.utils.interceptors.correlationID.CorrelationIdUtils;
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Getter
 @ControllerAdvice
 public class UtilsException extends Exception {
   private ExceptionCode exceptionCode;
+  private String exceptionMessage;
   /**
    * Constructs a new exception with {@code null} as its detail message. The cause is not
    * initialized, and may subsequently be initialized by a call to {@link #initCause}.
@@ -41,6 +44,19 @@ public class UtilsException extends Exception {
     this.exceptionCode = exceptionCode;
   }
 
+  /**
+   * Constructs a new exception with the specified detail message. The cause is not initialized, and
+   * may subsequently be initialized by a call to {@link #initCause}.
+   *
+   * @param message the detail message. The detail message is saved for later retrieval by the
+   *     {@link #getMessage()} method.
+   */
+  public UtilsException(ExceptionCode exceptionCode, String message, String exceptionMessage) {
+    super(message);
+    this.exceptionCode = exceptionCode;
+    this.exceptionMessage = exceptionMessage;
+  }
+
   public final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
   @ExceptionHandler(value = UtilsException.class)
@@ -62,20 +78,30 @@ public class UtilsException extends Exception {
               ? e.exceptionCode.getStatus()
               : GenericException.ERRDEFUTL001.getStatus());
       error.setUrl(request.getRequestURI().isBlank() ? null : request.getRequestURI());
-      if (e.exceptionCode.getMessage() != null
-          && e.getMessage() != null
-          && !e.exceptionCode.getMessage().isBlank()
-          && !e.getMessage().isBlank()) {
-        errorMes.setMessage(
-            "Message: " + e.exceptionCode.getMessage() + " | Exception Message: " + e.getMessage());
-        /*errorMes.setMessage(e.exceptionCode.getMessage());
-        errorMes.setExceptionMessage(e.getMessage());*/
+
+      if (e.getMessage() != null && !e.getMessage().isBlank()) {
+        errorMes.setMessage(e.getMessage());
+      } else errorMes.setMessage(e.exceptionCode.getMessage());
+
+      if (e.getExceptionMessage() != null && !e.getExceptionMessage().isBlank())
+        errorMes.setExceptionMessage(e.getExceptionMessage());
+
+      /*if (e.exceptionCode.getMessage() != null
+        && e.getMessage() != null
+        && !e.exceptionCode.getMessage().isBlank()
+        && !e.getMessage().isBlank()) {
+      */
+      /*errorMes.setMessage(
+      "Message: " + e.exceptionCode.getMessage() + " | Exception Message: " + e.getMessage());*/
+      /*
+        errorMes.setMessage(e.exceptionCode.getMessage());
+        errorMes.setExceptionMessage(e.getMessage());
       } else if (e.exceptionCode.getMessage() != null
               && !e.exceptionCode.getMessage().isBlank()
               && e.getMessage() == null
           || e.getMessage().isBlank()) {
         errorMes.setMessage(e.exceptionCode.getMessage());
-      } else errorMes.setMessage(e.getMessage());
+      } else errorMes.setMessage(e.getMessage());*/
       errorMes.setExceptionName(
           e.exceptionCode.exceptionName().isBlank() ? null : e.exceptionCode.exceptionName());
       error.setCorrelationId(CorrelationIdUtils.getCorrelationId());
@@ -116,8 +142,8 @@ public class UtilsException extends Exception {
             : GenericException.ERRDEFUTL001.name());
     errorMes.setStatus(status != null ? status : defaultStatus);
     if (e.getMessage() != null && !e.getMessage().isBlank()) {
-      errorMes.setMessage("Exception Message: " + e.getMessage());
-    } else errorMes.setMessage(null);
+      errorMes.setExceptionMessage(e.getMessage());
+    } else errorMes.setExceptionMessage(null);
     exceptionResponse.setUrl(
         request.getRequestURI() == null || request.getRequestURI().isBlank()
             ? null
@@ -133,7 +159,7 @@ public class UtilsException extends Exception {
     errorMes.setExceptionCode(GenericException.ERRDEFUTL001.name());
     errorMes.setStatus(GenericException.ERRDEFUTL001.getStatus());
     exceptionResponse.setCorrelationId(CorrelationIdUtils.getCorrelationId());
-    errorMes.setMessage(GenericException.ERRDEFUTL001.getMessage());
+    errorMes.setExceptionMessage(GenericException.ERRDEFUTL001.getMessage());
     errorMes.setExceptionName(GenericException.ERRDEFUTL001.exceptionName());
     exceptionResponse.setError(errorMes);
     return exceptionResponse;
