5458e562234940ea6ddc4bc0dd24943f33568553
Giovanni <giovannilamarmora.working@gmail.com>
2024-01-31 18:03:19 +0100

    feat(): Added OpenAPIConfig, bump 0.2.65

---
+++ src/main/java/io/github/giovannilamarmora/utils/config/OpenAPIConfig.java
@@ -0,0 +1,82 @@
+package io.github.giovannilamarmora.utils.config;
+
+import io.github.giovannilamarmora.utils.utilities.FilesUtils;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.Paths;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
+
+@Configuration
+public class OpenAPIConfig {
+
+  private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+  @Bean
+  public OpenApiCustomiser applyStandardOpenAPIModifications() {
+    return openApi -> {
+      Paths paths = new Paths();
+      openApi.getPaths().entrySet().stream()
+          .sorted(Map.Entry.comparingByKey())
+          .forEach(entry -> paths.addPathItem(entry.getKey(), addExamples(entry.getValue())));
+      openApi.setPaths(paths);
+    };
+  }
+
+  private PathItem addExamples(PathItem pathItem) {
+    if (!ObjectUtils.isEmpty(pathItem.getGet())) {
+      addOperations(pathItem.getGet());
+    }
+    if (!ObjectUtils.isEmpty(pathItem.getPost())) {
+      addOperations(pathItem.getPost());
+    }
+    if (!ObjectUtils.isEmpty(pathItem.getPatch())) {
+      addOperations(pathItem.getPatch());
+    }
+    if (!ObjectUtils.isEmpty(pathItem.getPut())) {
+      addOperations(pathItem.getPut());
+    }
+    if (!ObjectUtils.isEmpty(pathItem.getDelete())) {
+      addOperations(pathItem.getDelete());
+    }
+    return pathItem;
+  }
+
+  private void addOperations(Operation operation) {
+    if (!ObjectUtils.isEmpty(operation) && !ObjectUtils.isEmpty(operation.getResponses())) {
+      operation
+          .getResponses()
+          .forEach(
+              (statusCode, apiResponse) -> {
+                if (!ObjectUtils.isEmpty(apiResponse.getContent())) {
+                  apiResponse
+                      .getContent()
+                      .values()
+                      .forEach(
+                          content -> {
+                            try {
+                              String fileName = getExampleFileName(content.getExample().toString());
+                              LOG.debug("FileName is {}", fileName);
+                              String jsonContent = FilesUtils.searchFileFromResources(fileName);
+                              if (jsonContent != null) {
+                                content.setExample(jsonContent);
+                              }
+                            } catch (Exception e) {
+                              LOG.error(
+                                  "An Exception occurred during read filename for Open API", e);
+                            }
+                          });
+                }
+              });
+    }
+  }
+
+  private String getExampleFileName(String fileName) {
+    return fileName.replaceFirst("@", "");
+  }
+}
