66402f38c7f42cf97dde5c9e1c9ed7eabfd649cf
Giovanni <giovannilamarmora.working@gmail.com>
2024-07-21 18:06:46 +0200

    Utils V1.0.27

--- src/main/java/io/github/giovannilamarmora/utils/logger/MDCUtils.java
+++ src/main/java/io/github/giovannilamarmora/utils/logger/MDCUtils.java
@@ -1,5 +1,9 @@
 package io.github.giovannilamarmora.utils.logger;
 
+import static io.github.giovannilamarmora.utils.context.ContextConfig.*;
+import static io.github.giovannilamarmora.utils.context.ContextConfig.ENV;
+
+import io.github.giovannilamarmora.utils.context.TraceUtils;
 import io.github.giovannilamarmora.utils.interceptors.LogInterceptor;
 import io.github.giovannilamarmora.utils.interceptors.LogTimeTracker;
 import io.micrometer.context.ContextRegistry;
@@ -20,10 +24,19 @@ public abstract class MDCUtils {
     MDC.put(name, data);
   }
 
+  @LogInterceptor(type = LogTimeTracker.ActionType.UTILS_LOGGER)
   public static void registerMDC(String mdcKey) {
     Supplier<String> getMDC = () -> MDC.get(mdcKey);
     Consumer<String> putMDC = value -> MDC.put(mdcKey, value);
     Runnable removeMDC = () -> MDC.remove(mdcKey);
     ContextRegistry.getInstance().registerThreadLocalAccessor(mdcKey, getMDC, putMDC, removeMDC);
+  }
+
+  @LogInterceptor(type = LogTimeTracker.ActionType.UTILS_LOGGER)
+  public static void registerDefaultMDC(String env) {
+    MDC.put(TRACE_ID.getValue(), TraceUtils.getTraceID());
+    MDC.put(SPAN_ID.getValue(), TraceUtils.getSpanID());
+    MDC.put(PARENT_ID.getValue(), TraceUtils.getParentID());
+    if (!ObjectUtils.isEmpty(env)) MDC.put(ENV.getValue(), env);
   }
 }
