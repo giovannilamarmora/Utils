46a20f117a19b56ca1a9241d7d1ae2b9734771be
Giovanni <giovannilamarmora.working@gmail.com>
2025-02-19 18:32:50 +0100

    Utils V1.0.60

--- src/main/java/io/github/giovannilamarmora/utils/web/RequestManager.java
+++ src/main/java/io/github/giovannilamarmora/utils/web/RequestManager.java
@@ -2,23 +2,25 @@ package io.github.giovannilamarmora.utils.web;
 
 import io.github.giovannilamarmora.utils.interceptors.LogInterceptor;
 import io.github.giovannilamarmora.utils.interceptors.LogTimeTracker;
+import io.github.giovannilamarmora.utils.utilities.ObjectToolkit;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.util.ObjectUtils;
 
 public interface RequestManager {
 
   @LogInterceptor(type = LogTimeTracker.ActionType.UTILS_LOGGER)
   static String getCookieOrHeaderData(String name, ServerHttpRequest request) {
     String cookie = CookieManager.getCookie(name, request);
-    if (ObjectUtils.isEmpty(cookie)) return HeaderManager.getHeader(name, request);
-    return cookie;
+    String header = HeaderManager.getHeader(name, request);
+    if (ObjectToolkit.isNullOrEmpty(header)) return cookie;
+    return header;
   }
 
   @LogInterceptor(type = LogTimeTracker.ActionType.UTILS_LOGGER)
   static String getCookieOrHeaderData(
       String cookieName, String headerName, ServerHttpRequest request) {
     String cookie = CookieManager.getCookie(cookieName, request);
-    if (ObjectUtils.isEmpty(cookie)) return HeaderManager.getHeader(headerName, request);
-    return cookie;
+    String header = HeaderManager.getHeader(headerName, request);
+    if (ObjectToolkit.isNullOrEmpty(header)) return cookie;
+    return header;
   }
 }
