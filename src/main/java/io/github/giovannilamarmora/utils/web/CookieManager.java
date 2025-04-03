8abe4e0268b53f10204b55c36bb695b2803b27b9
Giovanni <giovannilamarmora.working@gmail.com>
2025-04-03 20:00:07 +0200

    Utils V1.0.70

--- src/main/java/io/github/giovannilamarmora/utils/web/CookieManager.java
+++ src/main/java/io/github/giovannilamarmora/utils/web/CookieManager.java
@@ -57,15 +57,16 @@ public interface CookieManager {
       ServerHttpResponse response,
       ServerHttpRequest request) {
 
-    String redirectUri = request.getQueryParams().getFirst("redirect_uri");
+    // String redirectUri = request.getQueryParams().getFirst("redirect_uri");
     // Extract domain from Referer or Origin (keeping URL handling as before)
     String referer = request.getHeaders().getFirst(HttpHeaders.REFERER);
     String origin = request.getHeaders().getFirst(HttpHeaders.ORIGIN);
     String host = request.getHeaders().getFirst(HttpHeaders.HOST);
-    String url =
-        (redirectUri != null)
-            ? redirectUri
-            : (origin != null && !origin.equals("*") ? origin : (host != null ? host : referer));
+    String url = (origin != null && !origin.equals("*")) ? origin : (host != null ? host : referer);
+    // String url =
+    //    (redirectUri != null)
+    //        ? redirectUri
+    //        : (origin != null && !origin.equals("*") ? origin : (host != null ? host : referer));
     String domain = WebManager.extractDomain(url);
 
     // Create the cookie with security attributes
